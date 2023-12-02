740359a8edc582a37e363be958b8b22df0219ead
Gowtham Raj <gowthrj@amazon.com>
2023-12-03 01:13:11 +0530

    Removed redundant file

--- src/test/java/com/amazon/Hawk/TestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.amazon.Hawk;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class TestSuite {
-	WebDriver driver = new ChromeDriver();
-	
-}
